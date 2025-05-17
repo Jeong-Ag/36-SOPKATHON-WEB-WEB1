1817b2efaebe2370a9cfae898e091bfbb276c688
yeeeww <yeangsshi@ewhain.net>
2025-05-17 21:56:28 +0900

    feat:theme.js 세팅

---
+++ src/pages/home/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const home = () => {
+  return <Wrapper>home</Wrapper>;
+};
+
+export default home;
+
+const Wrapper = styled.div`
+  background-color: ${theme.colors.gray1};
+  font: ${theme.typography.textStyles.h0_sb_22};
+`;

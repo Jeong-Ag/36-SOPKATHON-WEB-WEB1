774ab7cc46483475cb19ed5c60919f30a1924996
Jeong-Ag <kjungu1105@gmail.com>
2025-05-18 02:40:28 +0900

    feat: 온보딩 배경색 설정

---
+++ src/pages/home/onboarding/OnboardingSection.jsx
@@ -0,0 +1,7 @@
+import { Container } from './OnboardingSection.styles.js';
+
+const OnboardingSection = () => {
+  return <Container></Container>;
+};
+
+export default OnboardingSection;
---
+++ src/pages/home/onboarding/OnboardingSection.styles.js
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  width: 100%;
+  height: 667px;
+  background: linear-gradient(180deg, rgba(251, 255, 172, 0.87) 0%, #ffffff 84.62%);
+  display: flex;
+  padding: 1rem;
+`;

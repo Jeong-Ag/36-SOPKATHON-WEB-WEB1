0f0bc9efdb9efedc3d12ce6cda1d5f271b777a4a
Jeong-Ag <kjungu1105@gmail.com>
2025-05-18 05:28:34 +0900

    design: 버튼 컴포넌트에 비활성화 상태 추가 및 스타일 개선

--- src/shared/components/buttons/Button.styles.js
+++ src/shared/components/buttons/Button.styles.js
@@ -1,47 +1,38 @@
 import { css } from '@emotion/react';
-import theme from '@/shared/styles/theme';
+import theme, { typo } from '@/shared/styles/theme';
+
+const baseButtonStyle = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 0.625rem;
+  border: none;
+  text-align: center;
+  ${typo.h4_sb_17};
+`;
 
 export const buttonVariantStyles = {
   long: css`
-    display: flex;
+    ${baseButtonStyle};
     padding: 12px;
-    justify-content: center;
-    align-items: center;
-    gap: 0.625rem;
-    border-radius: 0.625rem;
-    border: none;
     background: ${theme.colors.potato_orange};
     color: ${theme.colors.white};
-    ${theme.typography.textStyles.h4_sb_17};
-    text-align: center;
     width: 335px;
   `,
 
-  shortActive: css`
-    display: flex;
-    width: 5.625rem;
-    padding: 0.75rem 6rem;
-    justify-content: center;
-    align-items: center;
-    gap: 0.625rem;
-    border-radius: 0.625rem;
+  short: css`
+    ${baseButtonStyle};
+    width: 90px;
+    padding: 12px 0;
     background: ${theme.colors.potato_orange};
     color: ${theme.colors.white};
-    ${theme.typography.textStyles.h4_sb_17};
-    text-align: center;
   `,
 
-  shortInactive: css`
-    display: flex;
-    width: 5.625rem;
-    padding: 0.75rem 6rem;
-    justify-content: center;
-    align-items: center;
-    gap: 0.625rem;
-    border-radius: 0.625rem;
+  shortDisabled: css`
+    ${baseButtonStyle};
+    width: 90px;
+    padding: 12px 0;
     background: ${theme.colors.gray4};
     color: ${theme.colors.gray8};
-    ${theme.typography.textStyles.h4_sb_17};
-    text-align: center;
   `,
 };

a5779b674335564805e7d8b77f0ecfda1158f4c4
Jeong-Ag <kjungu1105@gmail.com>
2025-05-17 23:51:10 +0900

    refactor: 글로벌 스타일에 Pretendard 폰트 추가 및 불필요한 .gitkeep 파일 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { RouterProvider } from 'react-router-dom';
 import theme from './shared/styles/theme';
-import GlobalStyle from '@/shared/styles/global';
+import GlobalStyle from './shared/styles/global';
 import { Global, ThemeProvider } from '@emotion/react';
 import Routes from './routes/Routes';
 import RankingDetail from './shared/components/RankingDetail';

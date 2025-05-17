024dabe3bf44525626dcbd6355df4c58d935dff3
Hyeseong Moon <coldute1014@naver.com>
2025-05-17 22:07:08 +0900

    fix: 라우팅 파일 구조 변경

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
-import pageRoute from '@/routes/pageRoute'
+import pageRoute from '@/routes/Routes'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
